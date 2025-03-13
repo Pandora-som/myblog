1f68189fc25f47379e7f670ac12b0bf2701dd9b8
Mari <testirovsik0005@mail.ru>
2025-03-13 21:37:41 +0300

    создала структуру базы данных

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+class Comment(models.Model):
+    name_comment = models.CharField(max_length=255)
+    comment_text = models.TextField()
+    date_comment = models.DateTimeField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class State(models.Model):
+    name_state = models.CharField(max_length=255)
+    state_text = models.TextField()
+    date_state = models.DateTimeField()
+    comment_id = models.ForeignKey(Comment, null=True, on_delete=models.SET_NULL)
+    
